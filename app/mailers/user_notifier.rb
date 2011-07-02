ef0f95bf42d7c6728db6c63799d073dea1372e04
Brian Gates <briandotgates@gmail.com>
2011-07-01 22:16:47 -0400

    goddamn email

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -6,24 +6,24 @@ class UserNotifier < ActionMailer::Base
     setup_email(user, school)
     @subject    += '| Login'
     @url = "http://#{school.domain_name}.littleredbrick.com/bypass/#{user.authorization.bypass_code}"
+    mail(:to => user.email)
   end
 
   def welcome_email(school, user)
     setup_email(user, school)
     @subject = "Thanks for signing up with LittleRedBrick!"
     @url = "http://#{school.domain_name}.littleredbrick.com"
-    mail(:from => "welcome@littleredbrick.com")
+    mail(:to => user.email, :from => "welcome@littleredbrick.com")
   end
 
   def school_creation_notification(school, user)
     setup_email(user, school)
-    @recipients = "bmathg@yahoo.com"
     @subject = "New school"
+    mail(:to => "briandotgates@gmail.com")
   end
   
   protected
   def setup_email(user, school)
-    @recipients  = "#{user.email}"
     @subject     = "LittleRedBrick "
     @sent_on     = Time.now
     @user, @school = user, school
